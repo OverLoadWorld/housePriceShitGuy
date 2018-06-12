56a8c766bbc737db0fa868260b30c411af9e2b5d
zhaozhenhua <zhaozhenhua1@xiaomi.com>
2018-06-12 20:49:17 +0800

    feat(fang): :sparkles: Extract data from fang.com of Datong

--- housePriceShitGuy/spiders/comFang_spider.py
+++ housePriceShitGuy/spiders/comFang_spider.py
@@ -1,29 +1,41 @@
 import scrapy
 
 
+def extract_type(infoItem, speItem, split):
+    ret = ''
+    typeList = infoItem.xpath("div[@class='clearfix']/div[@class='nlc_details']").xpath(speItem)
+    for type in typeList:
+        ret += "%s%s" % (type.extract().strip(), split)
+    return ret
+
+
 class ComFangSpider(scrapy.Spider):
     name = "fang"
-
-    def start_requests(self):
-        urls = [
-            'http://newhouse.datong.fang.com/house/s/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        'http://newhouse.datong.fang.com/house/s/',
+        # 'http://newhouse.datong.fang.com/house/s/b92/',
+        # 'http://newhouse.datong.fang.com/house/s/b93/',
+    ]
 
     def parse(self, response):
-        for infoItem in response.xpath("//div[@class='nl_con clearfix']/ul/li/div[@class='clearfix']/div[@class='nlc_details']"):
-            yield{
-                'name' : infoItem.xpath("div[@class='house_value clearfix']/div[@class='nlcd_name']/a/text()").extract_first(),
-                'type' : infoItem.xpath("//div[@class='house_type clearfix']//a/text()").extract_first(),
-                'address' : infoItem.xpath("//div[@class='address']//a/text()").extract_first(),
-                'tel' : infoItem.xpath("//div[@class='tel']//p//text()").extract_first(),
-                'price' : infoItem.xpath("//div[@class='nhouse_price']//text()").extract()
+        for infoItem in response.xpath(
+                "//div[@class='nl_con clearfix']/ul/li"):
+            name = infoItem.xpath("div[@class='clearfix']/div[@class='nlc_details']/div[@class='house_value clearfix']/div[@class='nlcd_name']/a/text()").extract_first()
+            price = infoItem.xpath("div[@class='clearfix']/div[@class='nlc_details']/div[@class='nhouse_price']/span/text()").extract_first()
+            unit = infoItem.xpath("div[@class='clearfix']/div[@class='nlc_details']/div[@class='nhouse_price']/em/text()").extract_first()
+            if name is None or price is None:
+                continue
+            yield {
+                'name': name.strip(),
+                'type': "%s %s" % (extract_type(infoItem, "div[@class='house_type clearfix']/text()", "-"),
+                                   extract_type(infoItem, "div[@class='house_type clearfix']/a/text()", "-")),
+                'address': infoItem.xpath("div[@class='clearfix']/div[@class='nlc_details']/div[@class='relative_message clearfix']/div[@class='address']/a/text()").extract_first().strip(),
+                'tel': "-".join(infoItem.xpath("div[@class='clearfix']/div[@class='nlc_details']/div[@class='relative_message clearfix']/div[@class='tel']/p/text()").extract()).strip(),
+                'price': "%s %s" % (price.strip(), "" if unit is None else unit.strip())
             }
 
-#        response.xpath("//div[@class='nl_con clearfix']/ul/li//div[@class='nlc_details']/div[@class='house_value clearfix']/div[@class='nlcd_name']/a/text()")
-#        page = response.url.split("/")[-2]
-#        filename = 'quotes-%s.html' % page
-#        with open(filename, 'wb') as f:
-#            f.write(response.body)
-#        self.log('Saved file %s' % filename)
+        next_page = response.xpath(
+                "//div[@class='page']/ul/li[@class='fr']/a[@class='next']/@href").extract_first()
+
+        if next_page is not None:
+            yield scrapy.Request(response.urljoin(next_page))
